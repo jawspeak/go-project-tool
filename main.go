98f7ea858b7138654935e2ef2a6fb9b083360cbb
Jonathan Wolter <jaw@squareup.com>
2015-09-03 06:09:39 -0700

    adding json parsing, with jsonq which maybe i dont want

--- main.go
+++ main.go
@@ -5,14 +5,53 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"crypto/tls"
+	"strings"
+	"encoding/json"
+	"github.com/jmoiron/jsonq"
+	"io/ioutil"
 )
 
 type Page struct {
 	Team string
 }
 
-func teamHandler(w http.ResponseWriter, r *http.Request) {
+func getJiraData() {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{
+		Transport: tr,
+	}
+	req, err := http.NewRequest("GET", "https://jira.corp.squareup.com/rest/api/2/filter/18720", nil)
+	req.SetBasicAuth("processing-bot", "n2nCmWF6")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	fmt.Println(resp.Status)
+	fmt.Println(resp.Header)
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := map[string]interface{}{}
+	
+	dec := json.NewDecoder(strings.NewReader(string(body)))
+	dec.Decode(&data)
+	jq := jsonq.NewQuery(data)
+
+	fmt.Println(jq)
+}
+
+// getStashData
+
+func teamHandler(w http.ResponseWriter, r *http.Request) {
+	getJiraData()
 	p := &Page{Team: "card processing"}
 	t, _ := template.ParseFiles("team.html")
 	t.Execute(w, p)
@@ -23,5 +62,4 @@ func main() {
 
 	http.HandleFunc("/team/card-processing", teamHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-	fmt.Println("bye")
 }
