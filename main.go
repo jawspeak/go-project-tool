3dac1b4c49b0806f54439bf78e5fac3bdb767ce1
Jonathan Wolter <jaw@squareup.com>
2015-12-20 13:56:09 -0800

    adding homepage

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	p := &Page{Title: "Developers Stash Dashboard"}
+	t, _ := template.ParseFiles("templates/index.thtml")
+	t.Execute(w, p)
+}
+
+func doNotCache(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Add("Pragma", "no-cache")
+		w.Header().Add("Expires", "0")
+		h.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	fmt.Println("started up!")
+	http.Handle("/static/", doNotCache(http.StripPrefix("/static", http.FileServer(http.Dir("static")))))
+	http.HandleFunc("/", indexHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
